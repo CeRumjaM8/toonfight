9a83d721b5976a7093e846d320600ab9b0ace92f
CeRumjaM8 <matheorocher053@gmail.com>
2025-06-21 19:53:37 +0200

    add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/**@type {importe('tailwindcss').Config}
+*/
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+  theme: {
+    extend:{},
+  },
+  plugins: [],
+};
